51a332eec3ce4c5bef5d909fe2e7742c63028bab
gitdangerous2000 <pedro.dias.pupo@gmail.com>
2023-09-27 20:50:51 -0300

    Update

---
+++ decrypt.py
@@ -0,0 +1,49 @@
+import os
+from cryptography.fernet import Fernet
+print("... libraries imported")
+
+
+# Collect files
+
+def get_file_list():
+	files = []
+	for file in os.listdir():
+		if file == "encrypt.py" or file == "decrypt.py" or file == "key_file.key":
+			continue
+		if os.path.isfile(file):
+			files.append(file)
+
+	print("Found files are:", files)
+	return files
+
+
+# GET Key
+
+def get_key():
+	with open("key_file.key", "rb") as f:
+		key = f.read()
+	print("...key found")
+	return key
+
+
+# Decrypt
+
+def decryption_process(files, key):
+    password = input("Digite a senha para descriptografar:")
+    if password == "senha":
+        print("Decryption started...")
+        for file in files:
+            with open(file, "rb") as f:
+                content = f.read()
+            decrypted_content = Fernet(key).decrypt(content)
+            with open(file, "wb") as f:
+                f.write(decrypted_content)
+                
+        print("...descriptografia completa")
+
+
+# function calls
+
+files = get_file_list()
+key = get_key()
+decryption_process(files, key)
